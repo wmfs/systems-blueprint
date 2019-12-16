137d01a6368a64ff22af4dec143e449773cafefd
Meena.Hoda <meena.hoda@wmfs.net>
2019-12-16 08:39:49 +0000

    refactor: remove clear users cache from this branch

--- functions/clear-users-cache.js
+++
@@ -1,8 +0,0 @@
-module.exports = function () {
-  return async function (env, event) {
-    const cacheService = env.bootedServices.caches
-    cacheService.reset('rolesFromUserId')
-    cacheService.reset('userMemberships')
-    return event
-  }
-}
