5fd3cb1d040b422e184bb01a6cb23613fd06c55b
reecebrend <brendy36@gmail.com>
2021-05-17 14:59:06 +0100

    feat: initial downlading capabilities

---
+++ functions/write-release-notes.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+const path = require('path')
+
+module.exports = function () {
+  return async function writeReleaseNotes(event) {
+    console.log('Write release notes incoming ', event)
+    const exportFilename = 'release-notes-test.txt'
+    fs.writeFileSync(path.join(event.exportDirectory, exportFilename), 'Test')
+
+    event.exportFilename = exportFilename
+
+    return event
+  }
+}
