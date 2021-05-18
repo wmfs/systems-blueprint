7ec6b585dce6c9586a129bfe03aa532c05c89e5f
reecebrend <brendy36@gmail.com>
2021-05-18 11:20:03 +0100

    feat: initial pdf exports

--- functions/write-release-notes.js
+++ functions/write-release-notes.js
@@ -1,14 +1,53 @@
-const fs = require('fs')
 const path = require('path')
+const Pdf = require('pdfkit')
+const fs = require('fs')
 
 module.exports = function () {
   return async function writeReleaseNotes (event) {
-    console.log('Write release notes incoming ', event)
-    const exportFilename = 'release-notes-test.txt'
-    fs.writeFileSync(path.join(event.exportDirectory, exportFilename), 'Test')
+    const { stories } = event
+    const notes = []
 
-    event.exportFilename = exportFilename
+    stories.forEach(story => {
+      notes.push(`${story.name} (https://app.clubhouse.io/wmfs/story/${story.id})`)
+    })
 
-    return event
+    const doc = new Pdf()
+    const today = new Date().toString().split(' ').slice(1, 4).join('-')
+    const exportFilename = 'release-notes-test.pdf'
+
+    const stream = doc.pipe(fs.createWriteStream(exportFilename))
+
+    doc.image(path.join(__dirname, '..', 'shared', 'Tymly-Logo.png'), {
+      fit: [250, 300],
+      align: 'top',
+      valign: 'left'
+    })
+
+    doc
+      .moveTo(75, 150)
+      .lineTo(500, 150)
+      .fillAndStroke('#878787')
+
+    doc
+      .moveTo(75, 175)
+      .lineTo(500, 175)
+      .fillAndStroke('#878787')
+
+    doc
+      .fontSize(10)
+      .text(`Versions released on ${today.toString().split(' ').slice(1, 4).join(' ')}`, 80, 160)
+
+    doc
+      .fontSize(12)
+      .text('Feature List:', 80, 215)
+
+    doc
+      .list(notes, 90, 240)
+
+    doc.end()
+    stream.on('finish', function () {
+      event.exportFilename = exportFilename
+      return event
+    })
   }
 }
