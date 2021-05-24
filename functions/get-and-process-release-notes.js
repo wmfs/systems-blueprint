6faa8c808625134395c44589a70ebc60e42ffc2c
meenahoda <meena.hoda@wmfs.net>
2021-05-24 18:18:18 +0100

    feat: display epic with release note

--- functions/get-and-process-release-notes.js
+++ functions/get-and-process-release-notes.js
@@ -1,32 +1,44 @@
-const { getStoriesByIteration } = require('../shared/clubhouse-api')
+const { getStoriesByIteration, getEpics } = require('../shared/clubhouse-api')
 const WORKFLOW_MAPPINGS = require('../shared/clubhouse-workflow-ids')
 
 module.exports = function () {
   return async function getAndProcessReleaseNotes (env, event) {
     const registry = env.bootedServices.registry
+    const token = registry.get('system_clubhouseToken')
 
     const READY_FOR_RELEASE_IDS = WORKFLOW_MAPPINGS
       .filter(workflow => workflow.state === 'Ready for Prod')
       .map(workflow => workflow.id)
 
-    const data = await getStoriesByIteration(registry.get('system_clubhouseToken'), event.iterationId)
+    const epics = await getEpics(token)
+    const stories = await getStoriesByIteration(token, event.iterationId)
 
     event.features = []
     event.bugs = []
     event.chores = []
 
-    data.forEach(story => {
+    stories.forEach(story => {
       const {
         id,
         name,
         description,
         workflow_state_id: workflowStateId,
-        story_type: storyType
+        story_type: storyType,
+        epic_id: epicId
+        // labels.map(r => r.name)
       } = story
 
-      const releaseNote = description && description.includes('<!-- Release Note -->')
-        ? `${description.split('<!-- Release Note -->')[1]} [ch${id}]`
-        : `${name} [ch${id}]`
+      const epic = epics.find(e => e.id === epicId)
+
+      let releaseNote = ''
+
+      if (epic && epic.name) releaseNote += `${epic.name}: `
+
+      releaseNote += description && description.includes('<!-- Release Note -->')
+        ? `${description.split('<!-- Release Note -->')[1]} `
+        : `${name} `
+
+      releaseNote += `[ch${id}]`
 
       if (READY_FOR_RELEASE_IDS.includes(workflowStateId)) {
         if (storyType === 'feature') event.features.push(releaseNote)
