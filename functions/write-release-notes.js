128ed061d3a89b717e93e64c0b98041361f8f3c2
meenahoda <meena.hoda@wmfs.net>
2021-05-25 10:02:46 +0100

    fix: asynchronous write release notes, and end write stream

--- functions/write-release-notes.js
+++ functions/write-release-notes.js
@@ -10,9 +10,9 @@ module.exports = function () {
     const now = env.bootedServices.timestamp.now()
 
     if (exportType === 'PDF') {
-      event.exportFilename = generatePdf(event, now)
+      event.exportFilename = await generatePdf(event, now)
     } else if (exportType === 'HTML') {
-      event.exportFilename = generateHtml(event, now)
+      event.exportFilename = await generateHtml(event, now)
     }
 
     return event
@@ -20,77 +20,88 @@ module.exports = function () {
 }
 
 function generatePdf (event, now) {
-  const { bugs, features, chores, endDate, exportDirectory } = event
+  return new Promise((resolve, reject) => {
+    const { bugs, features, chores, endDate, exportDirectory } = event
 
-  const exportFilename = `release-notes-${now.format('YYYYMMDD-HHmm')}.pdf`
+    const exportFilename = `release-notes-${now.format('YYYYMMDD-HHmm')}.pdf`
 
-  const doc = new PDFDocument({ compress: false })
+    const doc = new PDFDocument({ compress: false })
 
-  const stream = fs.createWriteStream(path.join(exportDirectory, exportFilename))
+    const stream = fs.createWriteStream(path.join(exportDirectory, exportFilename))
 
-  doc.pipe(stream)
+    doc.pipe(stream)
 
-  doc.image(
-    path.join(__dirname, '..', 'shared', 'Tymly-Logo.png'),
-    {
-      fit: [250, 300],
-      align: 'top',
-      valign: 'left'
+    doc.image(
+      path.join(__dirname, '..', 'shared', 'Tymly-Logo.png'),
+      {
+        fit: [250, 300],
+        align: 'top',
+        valign: 'left'
+      }
+    )
+
+    doc
+      .moveTo(75, 150)
+      .lineTo(500, 150)
+      .fillAndStroke('#878787')
+
+    doc
+      .moveTo(75, 175)
+      .lineTo(500, 175)
+      .fillAndStroke('#878787')
+
+    doc
+      .fontSize(10)
+      .text(`Release notes - Tymly release on ${endDate}`, 80, 160)
+
+    for (const [key, section] of Object.entries({ features, bugs, chores })) {
+      if (section.length > 0) {
+        doc.moveDown(2)
+
+        doc
+          .fontSize(12)
+          .text(`${Headers[key]}:`)
+
+        doc
+          .list(section)
+      }
     }
-  )
 
-  doc
-    .moveTo(75, 150)
-    .lineTo(500, 150)
-    .fillAndStroke('#878787')
+    doc.end()
 
-  doc
-    .moveTo(75, 175)
-    .lineTo(500, 175)
-    .fillAndStroke('#878787')
-
-  doc
-    .fontSize(10)
-    .text(`Release notes - Tymly release on ${endDate}`, 80, 160)
-
-  for (const [key, section] of Object.entries({ features, bugs, chores })) {
-    if (section.length > 0) {
-      doc.moveDown(2)
-
-      doc
-        .fontSize(12)
-        .text(`${Headers[key]}:`)
-
-      doc
-        .list(section)
-    }
-  }
-
-  doc.end()
-  // writeStream.on('finish', () => { .. your code here .. });
-  return exportFilename
+    stream
+      .on('finish', () => resolve(exportFilename))
+      .on('error', err => reject(err))
+  })
 } // generatePdf
 
 function generateHtml (event, now) {
-  const { bugs, features, chores, exportDirectory } = event
+  return new Promise((resolve, reject) => {
+    const { bugs, features, chores, exportDirectory } = event
 
-  const exportFilename = `release-notes-${now.format('YYYYMMDD-HHmm')}.html`
+    const exportFilename = `release-notes-${now.format('YYYYMMDD-HHmm')}.html`
 
-  let html = `<html lang="en"><head><meta charset="UTF-8"><title>${exportFilename}</title></head><body><h3>Release Notes:</h3><br>`
+    let html = `<html lang="en"><head><meta charset="UTF-8"><title>${exportFilename}</title></head><body><h3>Release Notes:</h3><br>`
 
-  for (const [key, section] of Object.entries({ features, bugs, chores })) {
-    if (section.length > 0) {
-      html += `<h3>${Headers[key]}:</h3><ul>`
-      section.forEach(story => {
-        html += `<li>${story}</li>`
-      })
-      html += '</ul>'
+    for (const [key, section] of Object.entries({ features, bugs, chores })) {
+      if (section.length > 0) {
+        html += `<h3>${Headers[key]}:</h3><ul>`
+        section.forEach(story => {
+          html += `<li>${story}</li>`
+        })
+        html += '</ul>'
+      }
     }
-  }
 
-  html += '</body></html>'
+    html += '</body></html>'
 
-  fs.writeFileSync(path.join(exportDirectory, exportFilename), html)
-
-  return exportFilename
+    fs.writeFile(
+      path.join(exportDirectory, exportFilename),
+      html,
+      err => {
+        if (err) reject(err)
+        else resolve(exportFilename)
+      }
+    )
+  })
 } // generateHtml
