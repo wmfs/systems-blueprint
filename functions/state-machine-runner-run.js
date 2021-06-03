98c9129fdec5514171c6e74d020b4db6f530daab
meenahoda <meena.hoda@wmfs.net>
2021-06-03 14:37:17 +0100

    feat: view result of state machine runner

--- functions/state-machine-runner-run.js
+++ functions/state-machine-runner-run.js
@@ -1,10 +1,10 @@
 module.exports = function () {
   return async function stateMachineRunnerRun (event, env, context) {
-    const { stateMachineName, input } = event.fields.params
+    const { stateMachineName, input } = event.fields.results[0].validated
     const { statebox } = env.bootedServices
     const { userId } = context
 
-    await statebox.startExecution(
+    event.execDesc = await statebox.startExecution(
       input ? JSON.parse(input) : {},
       stateMachineName,
       {
--- functions/state-machine-runner-validate.js
+++ functions/state-machine-runner-validate.js
@@ -2,6 +2,7 @@ module.exports = function () {
   return function stateMachineRunnerValidate (event, env) {
     const { statebox } = env.bootedServices
     const errors = []
+    const validated = {}
 
     const { stateMachineName, input: inputStr } = event
 
@@ -22,8 +23,13 @@ module.exports = function () {
       }
     }
 
+    if (!errors.length) {
+      validated.stateMachineName = stateMachineName
+      validated.input = inputStr
+    }
+
     return {
-      results: [{ errors }],
+      results: [{ errors, validated }],
       totalHits: 0
     }
   }
